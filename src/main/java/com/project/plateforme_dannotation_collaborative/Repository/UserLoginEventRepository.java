7cfa0733ae094a87f19918297119a3d3e9cbdfec
mehdi-haidri <mehdihaidri3@gmail.com>
2025-11-21 03:27:21 +0100

    final test 2

--- src/main/java/com/project/plateforme_dannotation_collaborative/Repository/UserLoginEventRepository.java
+++ src/main/java/com/project/plateforme_dannotation_collaborative/Repository/UserLoginEventRepository.java
@@ -11,13 +11,12 @@ import java.util.List;
 public interface UserLoginEventRepository extends JpaRepository<UserLoginEvent, Long> {
 
     @Query(
-            value = "SELECT FORMATDATETIME(login_time, 'yyyy-MM-dd') AS login_date, COUNT(*) AS count " +
+            value = "SELECT DATE_FORMAT(login_time, '%Y-%m-%d') AS login_date, COUNT(*) AS count " +
                     "FROM user_login_events " +
                     "WHERE login_time >= :fromDate " +
-                    "GROUP BY FORMATDATETIME(login_time, 'yyyy-MM-dd') " +
+                    "GROUP BY DATE_FORMAT(login_time, '%Y-%m-%d') " +
                     "ORDER BY login_date",
             nativeQuery = true
     )
     List<Object[]> getLoginStatsLast7Days(@Param("fromDate") LocalDateTime fromDate);
-
 }
